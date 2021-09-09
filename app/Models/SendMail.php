f3083de5175d25382b209801a05e9860d1790247
Nguyen Ngoc Thuan <thuannnth2009019@fpt.edu.vn>
2021-09-09 17:56:25 +0700

    fix code theo coding

--- app/Models/SendMail.php
+++ app/Models/SendMail.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class mail extends Model
