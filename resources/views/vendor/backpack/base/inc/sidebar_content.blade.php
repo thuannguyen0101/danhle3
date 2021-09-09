a0230bc4a10f40e11d6687203f7857877cabbb52
Nguyen Ngoc Thuan <thuannnth2009019@fpt.edu.vn>
2021-09-09 16:18:14 +0700

    t

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,26 +2,22 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i
             class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('department') }}'><i class='nav-icon la la-archive'></i>
-        Departments</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('team') }}'><i class='nav-icon la la-users'></i> Teams</a>
+        Phòng</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('team') }}'><i class='nav-icon la la-users'></i> Nhóm </a>
 </li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('team-detail') }}'><i
-            class='nav-icon la la-info-circle'></i> Team details</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('profile-work') }}'><i
-            class='nav-icon la la-pencil-square'></i> Profile works</a></li>
+            class='nav-icon la la-pencil-square'></i> Thông Tin Công Việc </a></li>
 
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('request') }}'><i class='nav-icon la la-question'></i>
-        Requests</a></li>
+        Gửi Yêu Cầu</a></li>
 <li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
+    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Quản Lý Người Dùng </a>
     <ul class="nav-dropdown-items">
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i>
-                <span>Users</span></a></li>
+                <span>Người Dùng</span></a></li>
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i
-                    class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
+                    class="nav-icon la la-id-badge"></i> <span>Vai trò</span></a></li>
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i
-                    class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
+                    class="nav-icon la la-key"></i> <span>Quyền</span></a></li>
     </ul>
 </li>
-
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('mail') }}'><i class='nav-icon la la-question'></i> Mails</a></li>
